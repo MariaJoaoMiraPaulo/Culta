cabb4386586f4934e6ebfe9bb905785a6c18dbc2
Francisca Paupério <franciscapauperio@AirdeFrancisca.home>
2022-12-28 19:53:09 +0000

    add services and imageSection banner

--- src/components/layouts/ServicesLayout.js
+++ src/components/layouts/ServicesLayout.js
@@ -1,14 +1,50 @@
 import * as React from 'react';
 import TitleSection from '../sections/TitleSection';
 import { withTrans } from '../../i18n/withTrans';
+import styled from 'styled-components';
+import { devices } from '../../styles/devices';
+import { SmallTitle } from '../../styles/typographyComponents';
+import ImageSection from '../sections/ImageSection';
 
-const ServicesLayout = ({ t }) => {
+const ServicesWrapper = styled.div`
+  display: grid;
+  margin: 0 2rem;
+  grid-template-columns: 100%;
+  padding: 20px;
+  justify-items: center;
+  @media ${devices.tablet} {
+    grid-template-columns: 50% 50%;
+  }
+`;
+
+const Service = styled.div`
+  color: ${({ theme }) => theme.colors.blue};
+  padding: 0 4rem;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  margin-bottom: 5rem;
+`;
+
+const ServicesLayout = ({ t, data, images }) => {
   return (
     <>
       <TitleSection
         title={t('services.services')}
         background="marble"
         color="blue"
+      />
+      <ServicesWrapper>
+        {data.map(({ node }) => (
+          <Service>
+            <SmallTitle>{node.serviceName}</SmallTitle>
+          </Service>
+        ))}
+      </ServicesWrapper>
+      <ImageSection
+        image={images['what.jpg']}
+        title={t('services.services')}
+        linkTo={'/services'}
       />
     </>
   );
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,13 +1,45 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 import LayoutWrapper from '../components/layouts/LayoutWrapper';
 import ServicesLayout from '../components/layouts/ServicesLayout';
+import { getImagesMappedByName } from '../utils/queryFunctions';
 
-const ServicesPage = () => {
+const ServicesPage = ({ data }) => {
+  const images = getImagesMappedByName(data);
+
   return (
     <LayoutWrapper logoColor="blue">
-      <ServicesLayout />
+      <ServicesLayout data={data.allContentfulServices.edges} images={images} />
     </LayoutWrapper>
   );
 };
 
 export default ServicesPage;
+
+export const query = graphql`
+  query MyQuery {
+    allContentfulServices {
+      edges {
+        node {
+          serviceName
+          serviceDescription {
+            serviceDescription
+          }
+        }
+      }
+    }
+    allImageSharp(
+      filter: { fluid: { originalName: { in: ["pilares.jpg", "what.jpg"] } } }
+    ) {
+      edges {
+        node {
+          id
+          gatsbyImageData
+          fluid {
+            originalName
+          }
+        }
+      }
+    }
+  }
+`;

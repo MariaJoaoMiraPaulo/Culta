f224fb55cccccefabce11984785a1f7f639e48d2
Maria João Mira Paulo <mariajoaomirapaulo1996@gmail.com>
2023-01-02 21:21:20 +0000

    Revert "Fix Menu"

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,34 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import { withTrans } from '../i18n/withTrans';
-import { navigate } from 'gatsby';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import MenuIcon from '../icons/MenuIcon';
 import CloseIcon from '../icons/CloseIcon';
-import { LinkWrapper } from './LinkWrapper';
+import MenuIcon from '../icons/MenuIcon';
 import { MenuTitle } from '../styles/typographyComponents';
+import { LinkWrapper } from './LinkWrapper';
+import { withTrans } from '../i18n/withTrans';
 
-const MenuContentWrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.marble};
-  position: fixed;
-  z-index: 1;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100vh;
-  overflow-y: hidden;
+const MenuWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 2rem;
+  padding: 1rem;
 `;
 
-const MenuActions = styled.div`
-  position: absolute;
-  right: 2rem;
-  top: 1rem;
-  z-index: 2;
+const MenuItems = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background-color: ${({ theme }) => theme.colors.marble};
+  padding: 2rem;
+  display: ${p => (p.open ? 'block' : 'none')};
+  z-index: 9;
+`;
+
+const AuxMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+`;
+
+const MenuHeader = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `;
 
 const MenuContent = styled.div`
-  bottom: 1rem;
-  right: 1.5rem;
-  position: absolute;
+  display: flex;
+  justify-content: flex-end;
   color: ${props => props.theme.colors.red};
 `;
 
@@ -40,73 +52,41 @@ const Links = styled.div`
 
 const Menu = ({ t }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const jumpTo = path => {
-    navigate(path);
-    setIsMenuOpen(false);
-  };
-
-  useEffect(() => {
-    if (isMenuOpen) {
-      document.querySelector('body').style.overflow = 'hidden';
-    } else {
-      document.querySelector('body').style.overflow = 'auto';
-    }
-  }, [isMenuOpen]);
-
   return (
     <>
-      <MenuActions>
-        {!isMenuOpen ? (
-          <MenuIcon onClickHandler={() => setIsMenuOpen(true)} />
-        ) : (
-          <CloseIcon color="red" onClickHandler={() => setIsMenuOpen(false)} />
-        )}
-      </MenuActions>
-      {isMenuOpen && (
-        <MenuContentWrapper>
+      <MenuWrapper>
+        {!isMenuOpen && <MenuIcon onClickHandler={() => setIsMenuOpen(true)} />}
+      </MenuWrapper>
+      <MenuItems open={isMenuOpen}>
+        <AuxMenu>
+          <MenuHeader>
+            <CloseIcon
+              color="red"
+              onClickHandler={() => setIsMenuOpen(false)}
+            />
+          </MenuHeader>
           <MenuContent>
             <Links>
-              <LinkWrapper
-                onClick={() => jumpTo('/about')}
-                color="red"
-                radius="25px"
-              >
+              <LinkWrapper to="/about" color="red" radius="25px">
                 <MenuTitle>{t('menu.about').toUpperCase()}</MenuTitle>
               </LinkWrapper>
-              <LinkWrapper
-                onClick={() => jumpTo('/services')}
-                color="red"
-                radius="25px"
-              >
+              <LinkWrapper to="/services" color="red" radius="25px">
                 <MenuTitle>{t('menu.services').toUpperCase()}</MenuTitle>
               </LinkWrapper>
-              <LinkWrapper
-                onClick={() => jumpTo('/blog')}
-                color="red"
-                radius="25px"
-              >
+              <LinkWrapper to="/blog" color="red" radius="25px">
                 <MenuTitle>{t('menu.stories').toUpperCase()}</MenuTitle>
               </LinkWrapper>
-              <LinkWrapper
-                onClick={() => jumpTo('/gallery')}
-                color="red"
-                radius="25px"
-              >
+              <LinkWrapper to="/gallery" color="red" radius="25px">
                 <MenuTitle>{t('menu.gallery').toUpperCase()}</MenuTitle>
               </LinkWrapper>
               {/* <LinkWrapper to="/" color="red" radius="25px"><MenuTitle>{t('portfolio').toUpperCase()}</MenuTitle></LinkWrapper> */}
-              <LinkWrapper
-                onClick={() => jumpTo('/contact')}
-                color="red"
-                radius="25px"
-              >
+              <LinkWrapper to="/contact" color="red" radius="25px">
                 <MenuTitle>{t('menu.contacts').toUpperCase()}</MenuTitle>
               </LinkWrapper>
             </Links>
           </MenuContent>
-        </MenuContentWrapper>
-      )}
+        </AuxMenu>
+      </MenuItems>
     </>
   );
 };

dc875d533626e9e48f907e7afc8879f1f340ab82
Maria João Mira Paulo <maria.joao@fidel.uk>
2023-02-13 08:25:04 +0000

    refactor(404): Refactor 404 error page

---
+++ src/components/ErrorLayout.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Caption, SubTitle } from '../styles/typographyComponents';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+import Header from '../components/Header';
+
+const CentralText = styled.div`
+  height: 100%;
+  width: 100%;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  flex-direction: column;
+`;
+
+const Page = styled.div`
+  background-color: ${({ theme }) => theme.colors['marble']};
+  max-width: 100%;
+  height: 100vh;
+`;
+
+const ErrorTitle = styled.div`
+  padding: 2rem 0;
+
+  span {
+    text-transform: uppercase;
+  }
+`;
+
+const CustomLink = styled(Link)`
+  color: ${({ theme }) => theme.colors['red']};
+  text-transform: uppercase;
+
+  &: hover {
+    color: transparent;
+    text-shadow: 0 0 10px red;
+  }
+`;
+
+const ErrorLayout = ({ title, description }) => {
+  return (
+    <Page>
+      <Header />
+      <CentralText>
+        <ErrorTitle>
+          <SubTitle color="red">{title}</SubTitle>
+        </ErrorTitle>
+        <Caption color="red">{description}</Caption>
+        <CustomLink to="/">Go Home</CustomLink>
+      </CentralText>
+    </Page>
+  );
+};
+
+export default ErrorLayout;
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,58 +1,16 @@
 import React from 'react';
-import { Caption, SubTitle } from '../styles/typographyComponents';
-import styled from 'styled-components';
-import { Link } from 'gatsby';
-import Header from '../components/Header';
+import ErrorLayout from '../components/ErrorLayout';
+import { withTrans } from '../i18n/withTrans';
 
-const CentralText = styled.div`
-  height: 100%;
-  width: 100%;
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  position: absolute;
-  top: 0;
-  flex-direction: column;
-`;
-
-const Page = styled.div`
-  background-color: ${({ theme, backgroundColor }) =>
-    theme.colors[backgroundColor] || 'none'};
-  max-width: 100%;
-  height: 100vh;
-`;
-
-const ErrorTitle = styled.div`
-  padding: 2rem 0;
-`;
-
-const CustomLink = styled(Link)`
-  color: ${({ theme }) => theme.colors['red']};
-  text-transform: uppercase;
-
-  &: hover {
-    color: transparent;
-    text-shadow: 0 0 10px red;
-  }
-`;
-
-const NotFoundPage = () => {
+const NotFoundPage = ({ t }) => {
   return (
-    <Page backgroundColor="marble">
-      <Header />
-      <CentralText>
-        <ErrorTitle>
-          <SubTitle color="red">PAGE NOT FOUND</SubTitle>
-        </ErrorTitle>
-        <Caption color="red">
-          Sorry we couldn't find what you were looking for.
-        </Caption>
-        <CustomLink to="/">Go Home</CustomLink>
-      </CentralText>
-    </Page>
+    <ErrorLayout
+      title={t('errors.404.title')}
+      description={t('errors.404.description')}
+    />
   );
 };
 
-export default NotFoundPage;
+export default withTrans(NotFoundPage);
 
 export const Head = () => <title>Not found</title>;
